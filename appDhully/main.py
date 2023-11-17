5f2c55a25373025985871017f9a05b9a872b9745
dhileanerodrigues <dhileane.rodrigues@sou.unijui.edu.br>
2023-11-17 09:47:38 -0300

    Alterando o git ignore

--- appDhully/main.py
+++ appDhully/main.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
       print("Press 1 to Alice encript your document in your module.")
       print("Press 2 to Bob encript your document in your module.")
       print("Press 3 Alice and bob send yours documents encripted.")
-      print("Press 0 to exit of sistem.")
+      print("Press 0 to exit of sistem. ")
 
       x = int(input())
       if x == 1:
